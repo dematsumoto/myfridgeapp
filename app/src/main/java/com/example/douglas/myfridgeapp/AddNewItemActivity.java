d4ebcd2815ab6a75c34f11b34cb114758a657e6a
Douglas Matsumoto <douglas.matsumoto@gmail.com>
2018-09-02 11:17:53 -0300

    changes to NewItemActivity: added back arrow, submition toast and layout adjustment

--- app/src/main/java/com/example/douglas/myfridgeapp/AddNewItemActivity.java
+++ app/src/main/java/com/example/douglas/myfridgeapp/AddNewItemActivity.java
@@ -2,19 +2,22 @@ package com.example.douglas.myfridgeapp;
 
 
 import android.support.v4.app.DialogFragment;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.douglas.myfridgeapp.util.DatePickerFragment;
 
 
-public class AddNewItemActivity extends FragmentActivity{
+public class AddNewItemActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_item);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 
     public void showDatePickerDialog(View v) {
@@ -22,6 +25,8 @@ public class AddNewItemActivity extends FragmentActivity{
         newFragment.show(getSupportFragmentManager(), "datePicker");
     }
 
+    public void showSubmitionToast(View v){
+        Toast.makeText(getApplicationContext(), "Adding item...[not implemented]", Toast.LENGTH_SHORT).show();
+    }
+
 }
-
-
